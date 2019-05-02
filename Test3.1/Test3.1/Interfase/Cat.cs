f2606d43450c131e775a073e72b3de2d73fc08fc
Karla Susana Mendoza Romero <A01566555@itesm.mx>
2019-05-02 13:34:38 -0600

    Initial Commit

---
+++ Test3.1/Test3.1/Interfase/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Test3._1.Interfase
+{
+    interface Cat:AnimalData
+    {
+        string Talk();
+    }
+}
---
+++ Test3.1/Test3.1/Interfase/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+   
+
+namespace Test3._1.Interfase
+{
+    interface Dog:AnimalData
+    {
+        string Talk();
+        
+    }
+}
